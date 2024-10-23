24f7a2a77697c4d75e2adb5e1a01d8171db96a68
tzcrowdis <100492586+tzcrowdis@users.noreply.github.com>
2024-10-23 15:44:46 -0600

    super user

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+
     # action post urls
     path("send_letter", views.send_crow_letter, name="send_crow_letter"),
 
@@ -11,7 +15,4 @@ urlpatterns = [
 
     # bureaucratic
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
 ]
